149fd2d7f5c08d028203757dd0148bb87b9b8f1e
radekor500 <radek00500@gmail.com>
2022-04-03 17:40:16 +0200

    docker support

--- FileSender/ClientApp/src/environments/environment.prod.ts
+++ FileSender/ClientApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7033/API/',
-  shareUrl: 'http://localhost:4200/files/'
+  apiUrl: 'http://localhost:5000/API/',
+  shareUrl: 'http://localhost:5000/files/'
 };
--- FileSender/EfModels/SendDbContext.cs
+++ FileSender/EfModels/SendDbContext.cs
@@ -21,10 +21,15 @@ namespace FileSender.EfModels
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            var dbPassword = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "Lotekcez@r1382";
+            var dbServer = Environment.GetEnvironmentVariable("DB_SERVER") ?? "localhost";
+            var dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "1433";
+            var dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "SA";
+
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server= 172.22.253.136,1433;Database=SendDb;Trusted_Connection=False;User ID=SA;Password=Lotekcez@r1382");
+                optionsBuilder.UseSqlServer($"Server={dbServer},{dbPort};Database=SendDb;Trusted_Connection=False;User ID={dbUser};Password={dbPassword}");
             }
         }
 
